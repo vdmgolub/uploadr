2f302fa41e9fb3245876bd3a69a58fdd12aa30d0
Vadim Golub <vdmgolub@gmail.com>
2014-08-31 18:49:23 +0300

    Image cropping functionality (without saving) added

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -5,5 +5,24 @@ $(function() {
   });
   $('#s3-uploader').bind('s3_upload_failed', function(e, content) {
     return alert(content.filename + ' failed to upload: ' + content.error_thrown);
+  }).bind('s3_upload_complete', function(e, content) {
+    $('#crop-image').attr('src', content.url);
+    $('#temp_url').val(content.url);
+    $('#crop-image').Jcrop({
+      onChange: update_crop,
+      onSelect: update_crop,
+      setSelect: [0, 0, 250, 250],
+      aspectRatio: 1
+    });
+
+    $('#uploader').hide();
+    $('#cropper').show();
   });
+
+  function update_crop(coords) {
+    $('#crop_x').val(coords.x);
+    $('#crop_y').val(coords.y);
+    $('#crop_w').val(coords.w);
+    $('#crop_h').val(coords.h);
+  }
 });
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,3 @@
 class Image < ActiveRecord::Base
+  attr_accessor :temp_url, :crop_x, :crop_y, :crop_w, :crop_h
 end
